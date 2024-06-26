a75d46184427e8af0e230633e1c94acf29aca133
kyko <59828706+rhrudska987@users.noreply.github.com>
2024-06-26 09:46:49 +0900

    [Silver V] Title: 올림픽, Time: 156 ms, Memory: 14756 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -12,18 +12,22 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 
-		int[][] nation = new int[N][2];
+		int[][] nation = new int[N][4];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			nation[i][0] = Integer.parseInt(st.nextToken());
-
-			int gold = Integer.parseInt(st.nextToken());
-			int silver = Integer.parseInt(st.nextToken());
-			int bronze = Integer.parseInt(st.nextToken());
-			nation[i][1] = 3 * gold + 2 * silver + 1 * bronze;
+			nation[i][1] = Integer.parseInt(st.nextToken());
+			nation[i][2] = Integer.parseInt(st.nextToken());
+			nation[i][3] = Integer.parseInt(st.nextToken());
 		}
 
 		Arrays.sort(nation, (o1, o2) -> {
+			if (o1[1] == o2[1]) {
+				if (o1[2] == o2[2]) {
+					return o2[3] - o1[3];
+				}
+				return o2[2] - o1[2];
+			}
 			return o2[1] - o1[1];
 		});
 
@@ -32,7 +36,7 @@ public class Main {
 			if (nation[i][0] == K) {
 				ans = i + 1;
 				for (int j = i - 1; j >= 0; j--) {
-					if (nation[i][1] == nation[j][1]) {
+					if (nation[i][1] == nation[j][1] && nation[i][2] == nation[j][2] && nation[i][3] == nation[j][3]) {
 						ans--;
 					}
 				}
