c229feeb4f02b55f379bf4ff920ece7beb3eb489
kyko <59828706+rhrudska987@users.noreply.github.com>
2024-07-05 16:21:44 +0900

    [Silver II] Title: 안녕, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.java
+++ 백준/Silver/1535. 안녕/안녕.java
@@ -1,26 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
 
 		int N = Integer.parseInt(br.readLine());
 		int[] loseStrength = new int[N];
 		int[] gainHappiness = new int[N];
 
-		st = new StringTokenizer(br.readLine());
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			loseStrength[i] = Integer.parseInt(st.nextToken());
-		}
-
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			gainHappiness[i] = Integer.parseInt(st.nextToken());
+			loseStrength[i] = Integer.parseInt(st1.nextToken());
+			gainHappiness[i] = Integer.parseInt(st2.nextToken());
 		}
 
 		int[] dp = new int[100];
