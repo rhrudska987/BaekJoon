288666393dd6cb506b0fedbe5a6c1c373ebb4d56
kyko <59828706+rhrudska987@users.noreply.github.com>
2024-08-05 09:18:12 +0900

    [Platinum V] Title: 찾기, Time: 512 ms, Memory: 67468 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -1,51 +1,61 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+	static int[] F;
+	static List<Integer> list = new ArrayList<>();
 
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String T = br.readLine();
 		String P = br.readLine();
-		int[] table = new int[P.length()];
 
-		for (int i = 1, j = 0; i < P.length(); i++) {
-			while (j > 0 && P.charAt(i) != P.charAt(j)) {
-				j = table[j - 1];
-			}
+		failureFunction(P);
+		KMP(T, P);
 
-			if (P.charAt(i) == P.charAt(j)) {
-				table[i] = ++j;
-			}
-		}
-
-		List<Integer> startPoint = new ArrayList<>();
-
-		for (int i = 0, j = 0; i < T.length(); i++) {
-			while (j > 0 && T.charAt(i) != P.charAt(j)) {
-				j = table[j - 1];
-			}
-
-			if (T.charAt(i) == P.charAt(j)) {
-				if (j == P.length() - 1) {
-					startPoint.add(i - j + 1);
-					j = table[j];
-				} else {
-					++j;
-				}
-			}
-		}
-
-		sb.append(startPoint.size() + "\n");
-		for (int i = 0; i < startPoint.size(); i++) {
-			sb.append(startPoint.get(i) + " ");
+		StringBuilder sb = new StringBuilder();
+		sb.append(list.size()).append("\n");
+		for (int i = 0; i < list.size(); i++) {
+			sb.append(list.get(i)).append("\n");
 		}
 
 		System.out.println(sb.toString());
 		br.close();
 	}
+
+	static void KMP(String text, String pattern) {
+		for (int i = 0, j = 0; i < text.length(); i++) {
+			while (j > 0 && pattern.charAt(j) != text.charAt(i)) {
+				j = F[j - 1];
+			}
+
+			if (pattern.charAt(j) == text.charAt(i)) {
+				if (j == pattern.length() - 1) {
+					list.add(i - j + 1);
+					j = F[j];
+				} else {
+					j++;
+				}
+			}
+		}
+
+	}
+
+	static void failureFunction(String pattern) {
+		F = new int[pattern.length()];
+
+		for (int i = 1, j = 0; i < pattern.length(); i++) {
+			while (j > 0 && pattern.charAt(j) != pattern.charAt(i)) {
+				j = F[j - 1];
+			}
+
+			if (pattern.charAt(j) == pattern.charAt(i)) {
+				F[i] = ++j;
+			}
+		}
+
+	}
+
 }
