b3339952cccda6a701834735bf4d920edb7e43ef
kyko <59828706+rhrudska987@users.noreply.github.com>
2024-08-21 08:46:45 +0900

    [Bronze V] Title: Hook, Time: 96 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		sb.append("#  # #### #### #  #").append("\n");
+		sb.append("#### #  # #  # # #").append("\n");
+		sb.append("#### #  # #  # # #").append("\n");
+		sb.append("#  # #### #### #  #").append("\n");
+		System.out.println(sb.toString());
+	}
+
+}
