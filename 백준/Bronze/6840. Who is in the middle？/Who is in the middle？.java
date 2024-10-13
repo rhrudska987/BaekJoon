4eae65be0781cb50adb9bb5571ead1ab497e78ff
kyko <59828706+rhrudska987@users.noreply.github.com>
2024-10-13 18:46:32 +0900

    [Bronze V] Title: Who is in the middle?, Time: 164 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+        int number3 = sc.nextInt();
+
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(number1);
+        al.add(number2);
+        al.add(number3);
+        Collections.sort(al);
+        System.out.println(al.get(1));
+    }
+}
