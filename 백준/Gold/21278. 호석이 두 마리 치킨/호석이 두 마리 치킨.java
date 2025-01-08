4565488482f4d6ef074a80405d5c746b9f1004a8
kyko <59828706+rhrudska987@users.noreply.github.com>
2025-01-08 16:19:56 +0900

    Update 호석이 두 마리 치킨.java

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
@@ -1,18 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
+	static int N;
+	static int road[][];
+
 	public static void main(String[] args) throws IOException {
+		readInput();
+		floydWarshall();
+		printAnswer();
+	}
+
+	static void readInput() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int[][] road = new int[N + 1][N + 1];
+		road = new int[N + 1][N + 1];
 
+		initRoad();
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			road[A][B] = road[B][A] = 2;
+		}
+
+		br.close();
+	}
+
+	private static void initRoad() {
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= N; j++) {
 				road[i][j] = 10000;
@@ -22,14 +43,9 @@ public class Main {
 				}
 			}
 		}
+	}
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			road[A][B] = road[B][A] = 2;
-		}
-
+	static void floydWarshall() {
 		for (int k = 1; k <= N; k++) {
 			for (int i = 1; i <= N; i++) {
 				for (int j = 1; j <= N; j++) {
@@ -37,40 +53,33 @@ public class Main {
 				}
 			}
 		}
-		
-		// for (int i = 1; i <= N; i++) {
-		// 	for (int j = 1; j <= N; j++) {
-		// 		System.out.print(road[i][j] + " ");
-		// 	}
-		// 	System.out.println();
-		// }
+	}
 
+	static void printAnswer() {
 		int[] answer = new int[2];
-		int minSum = Integer.MAX_VALUE;
-		int[] times = new int[N + 1];
+		int minTime = Integer.MAX_VALUE;
+
 		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				times[j] = road[i][j];
-			}
-
 			for (int j = i + 1; j <= N; j++) {
-				int sum = 0;
-				for (int k = 1; k <= N; k++) {
-					sum += Math.min(times[k], road[j][k]);
-				}
+				int totalTime = calculateTime(i, j);
 
-				if (sum < minSum) {
-					minSum = sum;
+				if (totalTime < minTime) {
+					minTime = totalTime;
 					answer[0] = i;
 					answer[1] = j;
 				}
 			}
 		}
-		System.out.println(answer[0] + " " + answer[1] + " " + minSum);
-		br.close();
 
+		System.out.println(answer[0] + " " + answer[1] + " " + minTime);
+	}
 
-
+	private static int calculateTime(int i, int j) {
+		int totalTime = 0;
+		for (int k = 1; k <= N; k++) {
+			totalTime += Math.min(road[i][k], road[j][k]);
+		}
+		return totalTime;
 	}
 
 }
