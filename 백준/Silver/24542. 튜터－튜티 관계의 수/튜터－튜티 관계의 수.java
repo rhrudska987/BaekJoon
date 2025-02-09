a3b3dbf42f6b937ec33fa0605e13320fb883adcf
kyko <59828706+rhrudska987@users.noreply.github.com>
2025-02-09 16:43:54 +0900

    [Silver I] Title: 튜터-튜티 관계의 수, Time: 424 ms, Memory: 60408 KB -BaekjoonHub

--- 백준/Silver/24542. 튜터－튜티 관계의 수/튜터－튜티 관계의 수.java
+++ 백준/Silver/24542. 튜터－튜티 관계의 수/튜터－튜티 관계의 수.java
@@ -1,59 +1,96 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] parent, size;
-    static final int MOD = 1_000_000_007;
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        
-        parent = new int[N + 1];
-        size = new int[N + 1];
-        
-        for (int i = 1; i <= N; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-        
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            union(u, v);
-        }
-        
-        long answer = 1;
-        for (int i = 1; i <= N; i++) {
-            if (parent[i] == i) {
-                answer = (answer * size[i]) % MOD;
-            }
-        }
-        
-        System.out.println(answer);
-    }
-    
-    static int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-    
-    static void union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
-        
-        if (rootA != rootB) {
-            if (size[rootA] < size[rootB]) {
-                parent[rootA] = rootB;
-                size[rootB] += size[rootA];
-            } else {
-                parent[rootB] = rootA;
-                size[rootA] += size[rootB];
-            }
-        }
-    }
+
+	static int N, M;
+	static int parent[];
+	static int childCnt[];
+
+	static final int MOD = 1_000_000_007;
+
+	public static void main(String[] args) throws Exception {
+		readInput();
+		long total = calcTotal();
+		printAnswer(total);
+	}
+
+	private static long calcTotal() {
+		long total = 1;
+
+		for (int i = 1; i <= N; i++) {
+			if (childCnt[i] == 0) {
+				continue;
+			}
+
+			total = (total * childCnt[i]) % MOD;
+		}
+
+		return total;
+	}
+
+	private static void printAnswer(long total) {
+		System.out.println(total % 1000000007);
+	}
+
+	private static void readInput() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		initParentAndChildCntArr();
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			union(u, v);
+		}
+
+		br.close();
+	}
+
+	private static void initParentAndChildCntArr() {
+		parent = new int[N + 1];
+		childCnt = new int[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+			childCnt[i] = 1;
+		}
+	}
+
+	private static boolean union(int x, int y) {
+		x = find(x);
+		y = find(y);
+
+		if (x == y) {
+			return false;
+		}
+
+		if (x <= y) {
+			parent[y] = x;
+			childCnt[x] += childCnt[y];
+			childCnt[y] = 0;
+		} else {
+			parent[x] = y;
+			childCnt[y] += childCnt[x];
+			childCnt[x] = 0;
+		}
+
+		return true;
+	}
+
+	private static int find(int x) {
+		if (parent[x] == x) {
+			return x;
+		}
+
+		return find(parent[x]);
+	}
+
 }
